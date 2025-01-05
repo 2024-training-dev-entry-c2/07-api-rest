2942b584745c49d5bce4dce7428f8b11a536fed6
Johan <johanvilla64@gmail.com>
2025-01-04 21:14:42 -0500

    feat: :sparkles: Add Dish Entity model

---
+++ src/main/java/com/example/restaurant/models/Dish.java
@@ -0,0 +1,33 @@
+package com.example.restaurant.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Dish {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String name;
+
+  @Column(nullable = false)
+  private Float price;
+
+  @ManyToOne(targetEntity = Menu.class, fetch = FetchType.LAZY)
+  private Menu menu;
+
+}
