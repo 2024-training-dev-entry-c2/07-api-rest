aeb2c314f3cd10da4206846a8fc1820b26621d79
Johan <johanvilla64@gmail.com>
2025-01-07 13:41:03 -0500

    fix: :bug: solve issues with observers and use logger instead of System.out.printLn

--- src/main/java/com/example/restaurant/services/observers/PopularDishObserver.java
+++ src/main/java/com/example/restaurant/services/observers/PopularDishObserver.java
@@ -4,20 +4,23 @@ import com.example.restaurant.models.Dish;
 import com.example.restaurant.models.Order;
 import com.example.restaurant.repositories.DishRepository;
 import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class PopularDishObserver implements OrderObserver {
 
+  private static final Logger log = LogManager.getLogger(PopularDishObserver.class);
   private final DishRepository dishRepository;
 
   @Override
   public void onOrderCreated(Order order) {
     for (Dish dish : order.getDishes()) {
-      Long dishOrdersQuantity = dishRepository.countOrdersByDishId(dish.getId());
+      Long dishOrdersQuantity = dishRepository.countOrdersById(dish.getId());
       Boolean isPopularDish = dishOrdersQuantity >= 100;
-      System.out.println(isPopularDish + dish.getName() + " es popular (ha sido ordenado más de 100 veces)");
+      log.info("{}{} es popular (ha sido ordenado más de 100 veces)", isPopularDish, dish.getName());
     }
   }
 }
--- src/main/java/com/example/restaurant/services/observers/RegularCustomerObserver.java
+++ src/main/java/com/example/restaurant/services/observers/RegularCustomerObserver.java
@@ -3,18 +3,21 @@ package com.example.restaurant.services.observers;
 import com.example.restaurant.models.Order;
 import com.example.restaurant.repositories.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class RegularCustomerObserver implements OrderObserver {
 
+  private static final Logger log = LogManager.getLogger(RegularCustomerObserver.class);
   private final OrderRepository orderRepository;
 
   @Override
   public void onOrderCreated(Order order) {
-    Long orderQuantity =  orderRepository.countByCustomerId(order.getCustomer().getId());
+    Long orderQuantity = orderRepository.countByCustomerId(order.getCustomer().getId());
     Boolean isRegularCustomer = orderQuantity >= 10;
-    System.out.println(isRegularCustomer + order.getCustomer().getName() + " es cliente regular (ha ordenado más de 10 veces): ");
+    log.info("{}{}Es cliente regular (ha ordenado más de 10 veces): ", isRegularCustomer, order.getCustomer().getName());
   }
 }
