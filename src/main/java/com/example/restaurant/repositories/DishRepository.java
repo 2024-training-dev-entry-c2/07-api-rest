45d704a79ac870edddf50c1783dc203b32c98a1d
Johan <johanvilla64@gmail.com>
2025-01-07 13:42:51 -0500

    fix: :bug: rename Dish repo method for for counting orders by dish "id"

--- src/main/java/com/example/restaurant/repositories/DishRepository.java
+++ src/main/java/com/example/restaurant/repositories/DishRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface DishRepository extends JpaRepository<Dish, Long> {
 
-    long countOrdersByDishId(Long dishId);
+    long countOrdersById(Long id);
 }
