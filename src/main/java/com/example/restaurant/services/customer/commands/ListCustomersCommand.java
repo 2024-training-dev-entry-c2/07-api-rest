edbd91e2dadc317c45171c2fe94c96d4ff647046
Johan <johanvilla64@gmail.com>
2025-01-10 06:30:28 -0500

    refactor:  :recycle: change the method invoke for parsing Customer responseDTO into list, needed for tests

--- src/main/java/com/example/restaurant/services/customer/commands/ListCustomersCommand.java
+++ src/main/java/com/example/restaurant/services/customer/commands/ListCustomersCommand.java
@@ -1,12 +1,14 @@
 package com.example.restaurant.services.customer.commands;
 
 import com.example.restaurant.mappers.CustomerMapper;
+import com.example.restaurant.models.Customer;
 import com.example.restaurant.models.dto.customer.CustomerResponseDTO;
 import com.example.restaurant.repositories.CustomerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -16,8 +18,9 @@ public class ListCustomersCommand {
   private final CustomerMapper customerMapper;
 
   public List<CustomerResponseDTO> execute() {
-    return customerRepository.findAll().stream()
-            .map(customerMapper::toDTO)
-            .toList();
+    List<Customer> customersEntities = customerRepository.findAll();
+
+    return customersEntities.stream()
+            .map(customerMapper::toDTO).collect(Collectors.toList());
   }
 }
